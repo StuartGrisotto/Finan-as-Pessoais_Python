27e3fb3443ce8d95301c5234a931420b05a044cd
StuartGrisotto <grisotto.work@gmail.com>
2024-10-09 17:55:02 -0300

    New Commit

--- components/sidebar.py
+++ components/sidebar.py
@@ -24,10 +24,8 @@ layout = dbc.Col([
 #seção de perfil--------------------
 dbc.Button(id='botão_avatar',
         children=[html.Img(src='/assent/img_hom.png', id='avatar_change', alt='Avatar', className='perfil_avatar')
-                ], style={'background-color': 'tranparent', 'border-color': 'transparent'})
-    ])
-
-
+                ], style={'background-color': 'tranparent', 'border-color': 'transparent'}),
+    
 #seção NOVO--------------------
 dbc.Row([
     dbc.Col([
@@ -36,12 +34,22 @@ dbc.Row([
 
     ], width=6),
     dbc.Col([
-        dbc.Button(color='sucess', id='open-novo-despesa',
+        dbc.Button(color='danger', id='open-novo-despesa',
             children=['+ Despesa'])
             
             
     ],width=6)
+      
+]),
+#seção NAV--------------------
+html.Hr(),
+dbc.Nav([
+    dbc.NavLink("Dashboard", href="/dashboard", active="exact"),
+    dbc.NavLink("Extratos", href="/extratos", active="exact"),
+], vertical=True, pills=True, id='nav.buttons', style={"margin-buttom": "50px"}),
+
 ])
+
 
 
 # =========  Callbacks  =========== #

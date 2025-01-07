cdfbcfa790471be98659de7a0ff9305bea96b90c
StuartGrisotto <grisotto.work@gmail.com>
2025-01-07 20:00:07 -0300

    New commit

--- components/dashboards.py
+++ components/dashboards.py
@@ -102,7 +102,7 @@ layout = dbc.Col([
                 dcc.DatePickerRange(
                     month_format='Do MMM, YY',
                     end_date_placeholder_text='Data...',
-                    start_date=datetime(2022, 1, 1).date(),
+                    start_date=datetime(2025, 1, 1).date(),
                     end_date=datetime.today() + timedelta(days= 31),
                     updatemode='singledate',
                     id='date-picker-config',
@@ -153,32 +153,83 @@ def populate_dropdownvalues(data):
     return [([{"label": x, "value": x} for x in df.Categoria.unique()]), val, f"R$ {valor}"]
 
 
+# VALOR - saldo
+@app.callback(
+    Output("p-saldo-dashboards", "children"),
+    [Input("store-despesas", "data"),
+    Input("store-receitas", "data")])
+def saldo_total(despesas, receitas):
+    df_despesas = pd.DataFrame(despesas)
+    df_receitas = pd.DataFrame(receitas)
+
+    valor_saldo = df_receitas['valor'].sum() - df_despesas['valor'].sum()
+    return f"R$ {valor_saldo}"
 
 
 @app.callback(
-    Output('graph1', 'figure')
+    Output('graph1', 'figure'),
     [Input('store-despesas', 'data'),
-     Input('store-receita', 'data'),
+     Input('store-receitas', 'data'),
      Input("dropdown-despesa", "value"),
      Input("dropdown-receita", "value"),]
 )
-
 def update_output(data_despesa, data_receita, despesa, receita):
+    # Converte os dados para DataFrame
+    df_despesas = pd.DataFrame(data_despesa).set_index("Data")[["valor"]]
+    df_ds = df_despesas.groupby("Data").sum().rename(columns={"valor": "Despesa"})
 
-    df_despesas = pd.DataFrame(data_despesa).set_index("Data") [["Valor"]]
-    df_ds = df_despesas.groupby("Data").sum().rename(columns={"Valor": "Despesa"})
-    
-    df_receitas = pd.DataFrame(data_receita).set_index("Data") [["Valor"]]
-    df_rc = df_despesas.groupby("Data").sum().rename(columns={"Valor": "Receita"})
+    df_receitas = pd.DataFrame(data_receita).set_index("Data")[["valor"]]
+    df_rc = df_receitas.groupby("Data").sum().rename(columns={"valor": "Receita"})
 
+    # Junta os dados e calcula o acumulado
     df_acum = df_ds.join(df_rc, how="outer").fillna(0)
     df_acum["Acum"] = df_acum["Receita"] - df_acum["Despesa"]
     df_acum["Acum"] = df_acum["Acum"].cumsum()
-    
 
+    # Cria o gráfico
     fig = go.Figure()
     fig.add_trace(go.Scatter(name="Fluxo de caixa", x=df_acum.index, y=df_acum["Acum"], mode="lines"))
-    
-    fig.update_layout(margin=graph_margin)
-    
+
+    fig.update_layout(margin=graph_margin, height=400)
+    fig.update_layout(paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)')
     return fig
+
+
+
+
+@app.callback(
+    Output('graph2', 'figure'),
+
+    [Input('store-receitas', 'data'),
+     Input('store-despesas', 'data'),
+     Input("dropdown-receita", "value"),
+     Input("dropdown-despesa", "value"),
+     Input('date-picker-config', 'start_date'),
+     Input('date-picker-config', 'end_date'), ]
+)
+def graph2_show(data_receita, data_despesa, receita, despesa, start_date, end_date):
+    df_ds = pd.DataFrame(data_despesa)
+    df_rc = pd.DataFrame(data_receita)
+
+    df_ds["Output"] = "Despesas"
+    df_rc["Output"] = "Receitas"
+
+    df_final = pd.concat([df_ds, df_rc]) #concatenação
+
+    df_final["Data"] = pd.to_datetime(df_final["Data"]) #formatação data, no string
+    start_date = pd.to_datetime(start_date) #formatação data, no string
+    end_date = pd.to_datetime(end_date) #formatação data, no string
+
+    df_final = df_final[(df_final["Data"] >= start_date) & (df_final["Data"] <= end_date)]
+    df_final = df_final[(df_final["Categoria"].isin(receita)) | (df_final["Categoria"].isin(despesa))]
+
+    fig = px.bar(df_final, x="Data", y="valor", color='Output', barmode="group")
+
+    fig.update_layout(margin=graph_margin, height=400)
+    fig.update_layout(paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)')
+    return fig
+
+
+@app.callback(
+    
+)
--- globals.py
+++ globals.py
@@ -1,42 +1,35 @@
 import pandas as pd
-import os 
+import os
 
+# Verifica e carrega arquivos de despesas e receitas
 if ("df_despesas.csv" in os.listdir()) and ("df_receitas.csv" in os.listdir()):
-    df_despesas = pd.read_csv("df_despesas.csv", index_col=0, parse_dates=True)
-    df_receitas = pd.read_csv("df_receitas.csv", index_col=0, parse_dates=True)
+    df_despesas = pd.read_csv("df_despesas.csv", parse_dates=["Data"])
+    df_receitas = pd.read_csv("df_receitas.csv", parse_dates=["Data"])
 
-    df_despesas["Data"] = pd.to_datetime(df_despesas["Data"], format='%Y-%m-%d')
-    df_receitas["Data"] = pd.to_datetime(df_receitas["Data"], format='%Y-%m-%d')
 
-    df_despesas["Data"] = df_despesas["Data"].apply(lambda x: x.date())
-    df_receitas["Data"] = df_receitas["Data"].apply(lambda x: x.date()) 
 
 
 else:
-    data_structure = {'valor': [],
-                    'Efetuado': [],
-                    'Fixo': [],
-                    'Data': [],
-                    'Categoria': [],
-                    'Descrição': []}
+    data_structure = {'valor': [], 'Efetuado': [], 'Fixo': [], 'Data': [], 'Categoria': [], 'Descrição': []}
     df_despesas = pd.DataFrame(data_structure)
     df_receitas = pd.DataFrame(data_structure)
-    df_despesas.to_csv("df_despesas.csv")
-    df_receitas.to_csv("df_receitas.csv")
+    df_despesas.to_csv("df_despesas.csv", index=False)
+    df_receitas.to_csv("df_receitas.csv", index=False)
 
 
 
+    
 
+# Verifica e carrega categorias
 if ("df_cat_receita.csv" in os.listdir()) and ("df_cat_despesa.csv" in os.listdir()):
-    df_cat_despesa = pd.read_csv("df_cat_despesa.csv", index_col=0)
-    df_cat_receita = pd.read_csv("df_cat_receita.csv", index_col=0)
-    cat_receita = df_cat_receita['Categoria'].tolist()  # Pega apenas a coluna 'Categoria' como lista
-    cat_despesa = df_cat_despesa['Categoria'].tolist()  # Pega apenas a coluna 'Categoria' como lista
+    df_cat_receita = pd.read_csv("df_cat_receita.csv")
+    df_cat_despesa = pd.read_csv("df_cat_despesa.csv")
+    cat_receita = df_cat_receita['Categoria'].tolist()
+    cat_despesa = df_cat_despesa['Categoria'].tolist()
 else:
-    cat_receita = {'Categoria': ["Salário", "Investimento", "Comissão"]}
-    cat_despesa = {'Categoria': ["Alimentação", "Aluguel", "Gasolina", "Saúde", "Lazer"]}
-
-    df_cat_receita = pd.DataFrame(cat_receita)
-    df_cat_despesa = pd.DataFrame(cat_despesa) 
-    df_cat_receita.to_csv("df_cat_receita.csv")
-    df_cat_despesa.to_csv("df_cat_despesa.csv")
+    cat_receita = ["Salário", "Investimento", "Comissão"]
+    cat_despesa = ["Alimentação", "Aluguel", "Gasolina", "Saúde", "Lazer"]
+    df_cat_receita = pd.DataFrame({"Categoria": cat_receita})
+    df_cat_despesa = pd.DataFrame({"Categoria": cat_despesa})
+    df_cat_receita.to_csv("df_cat_receita.csv", index=False)
+    df_cat_despesa.to_csv("df_cat_despesa.csv", index=False)
